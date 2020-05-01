5a9611d43b4ecfd9afbc0fbe23377bc9d4a99440
Seva <vsdudakov@gmail.com>
2020-05-02 00:43:21 +0300

    Moved port and host to config (#4)

--- core/settings.py
+++ core/settings.py
@@ -1,8 +1,9 @@
 import os
 
 AMQP_URL = os.environ["AMQP_URL"]
+HOST = os.environ["SOCKJS_HOST"]
+PORT = os.environ["SOCKJS_PORT"]
+
 SESSION_LIFE = 7200
 DEBUG = False
 MAX_LINE_SIZE = 8190 * 10
-HOST = "0.0.0.0"
-PORT = 8080
